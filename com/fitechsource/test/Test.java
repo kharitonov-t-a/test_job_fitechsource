faf753ca745dc2b7d5e7bf3cb0e34198cbd0b85f
kharitonov-t-a <kharitonov.t.a@yandex.ru>
2019-02-09 13:43:59 +0300

    add parallel calculation

--- com/fitechsource/test/Test.java
+++ com/fitechsource/test/Test.java
@@ -8,20 +8,75 @@ import java.util.*;
  * 1. Change this file or create new one using parallel calculation mode.
  * 2. Do not use `executors`, only plain threads  (max threads count which should be created for calculations is com.fitechsource.test.TestConsts#MAX_THREADS)
  * 3. Try to provide simple solution, do not implement frameworks.
- * 4. Don't forget that calculation method can throw exception, process it right way. 
+ * 4. Don't forget that calculation method can throw exception, process it right way.
  *   (Stop calculation process and print error message. Ignore already calculated intermediate results, user doesn't need it.)
- *   
+ *
  *   Please attach code files to email - skhisamov@fitechsource.com
  */
 
 public class Test {
-    public static void main(String[] args) throws TestException {
-        Set<Double> res = new HashSet<>();
+    private static volatile Set<Double> res = new HashSet<>();
+    private static volatile Counter counter = new Counter();
+    static List<Thread> threads = new ArrayList<Thread>();
+    /**
+     * testExceptionFlag
+     */
+    private static volatile boolean ignoreResult = false;
 
-        for (int i = 0; i < com.fitechsource.test.TestConsts.N; i++) {
-            res.addAll(TestCalc.calculate(i));
+    public static void main(String[] args) throws TestException {
+//        long start = System.nanoTime();
+
+        for (int i = 0; i < TestConsts.MAX_THREADS; i++) {
+            threads.add(new Thread(() -> {
+                try {
+                    for (int j = counter.increment(); j < TestConsts.N; j = counter.increment()){
+                        res.addAll(TestCalc.calculate(j));
+                    }
+                } catch (TestException e) {
+                    if(!ignoreResult){
+                        counter.set(TestConsts.N);
+                        ignoreResult = true;
+
+                        for (Thread thread : threads) {
+                            if(!thread.isInterrupted()) {
+                                thread.interrupt();
+                            }
+                        }
+                        e.printStackTrace();
+                        System.out.println(e.getMessage());
+                    }
+
+                }
+            }));
         }
 
-        System.out.println(res);
+        for (Thread thread: threads) {
+            thread.start();
+        }
+        for (Thread thread: threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+//        long finish = System.nanoTime();
+//        long timeConsumedMillis = finish - start;
+//        System.out.println(timeConsumedMillis);
+
+        if(!ignoreResult)
+            System.out.println(res);
+    }
+
+
+    public static class Counter {
+        private int counter = -1;
+
+        public synchronized int  get()      { return counter; }
+        public synchronized int set(int n) { return counter = n; }
+        public synchronized int increment() {
+            return set(get() + 1);
+        }
     }
 }
